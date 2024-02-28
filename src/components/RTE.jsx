226feb5d21fe120fb5dcdea13297f61ce4f18ae3
QaisarMoin <126318980+QaisarMoin@users.noreply.github.com>
2024-02-28 14:39:12 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -44,7 +44,6 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
               content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-              ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
             }}
             onEditorChange={onChange}
           />
